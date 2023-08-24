c0127ea970544f7d92914ab3ece46d1b578f5fc0
Karen Gasparyan <kgasparyan@aligntech.com>
2023-08-24 11:47:06 +0400

    homework 2

--- src/classwork/chapter2/ForSample.java
+++ src/classwork/chapter2/ForSample.java
@@ -2,11 +2,11 @@ package classwork.chapter2;
 
 public class ForSample {
     public static void main(String[] args) {
-        int i;
-        for(i = 0; i < 100; i++) {
+
+        for(int i = 0; i < 100; i++) {
             System.out.println("poxos");
         }
-        System.out.println(i);
+
         System.out.println("petros");
     }
 }
---
+++ src/classwork/chapter3/VariableExample.java
@@ -0,0 +1,9 @@
+package classwork.chapter3;
+
+public class VariableExample {
+
+    public static void main(String[] args) {
+        char a = '%';
+        System.out.println(a);
+    }
+}
